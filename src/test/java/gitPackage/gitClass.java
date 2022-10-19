5713562458ce2201098e4625fed8ee5b952db25a
Muhammed Emin Yilmaz <m.ylmz@yahoo.com>
2022-10-19 19:54:10 +0300

    Added some codes

--- src/test/java/gitPackage/gitClass.java
+++ src/test/java/gitPackage/gitClass.java
@@ -3,5 +3,9 @@ package gitPackage;
 public class gitClass {
     public static void main(String[] args) {
         System.out.println("git calismalari kapsaminda olusturuldu");
+        System.out.println("ikinci saitirimda cestili kodlar yazdim");
+        String name = "muhammed";
+        String surname = "yilmaz";
+        int doorNu= 14;
     }
 }
