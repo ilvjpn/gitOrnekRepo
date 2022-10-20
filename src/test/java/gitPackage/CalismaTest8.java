199a1661471ff0015f12649ead2a44304769c547
Muhammed Emin Yilmaz <m.ylmz@yahoo.com>
2022-10-20 19:14:04 +0300

    added new class and refactord

---
+++ src/test/java/gitPackage/CalismaTest8.java
@@ -0,0 +1,12 @@
+package gitPackage;
+
+public class CalismaTest8 {
+
+
+    // Bugun acil calsma kapsaminda istenen testleri burada hazirliyorum
+
+
+
+
+
+}
--- src/test/java/gitPackage/Class02.java
+++ src/test/java/gitPackage/Class02.java
@@ -1,4 +1,9 @@
 package gitPackage;
 
 public class Class02 {
+
+    int yas = 30;
+    String name = "Jhon";
+    String surname = "Doe";
+
 }
